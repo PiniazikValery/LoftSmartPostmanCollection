ca66b9d335a8eed348c667975535be2dd7aaf786
ICX\valery.piniazik <valery.piniazik@itechart-group.com>
2019-08-09 15:30:42 +0300

    CU - updated postman scripts and removed unnecessary packages from project

--- postman_test_runner.js
+++ postman_test_runner.js
@@ -1,33 +1,19 @@
 const newman = require('newman');
-const editJsonFile = require('edit-json-file');
 
 const pathToSignUpCollection = './Smoke.postman_collection.json';
 
-let Sign_Up_collection = editJsonFile(pathToSignUpCollection);
-
-let arrayOfParams = Sign_Up_collection.get('variable');
-
-arrayOfParams = arrayOfParams.map(param => {
-    if (param.key === 'email') {
-        param.value = `valery.piniazik+${new Date().getTime()}@yandex.by`;
-    }
-    return param;
-});
-
-Sign_Up_collection.set('variable', arrayOfParams);
-
-Sign_Up_collection.save(() => newman.run({
+newman.run({
     collection: require(pathToSignUpCollection),
-    reporters: ['cli','junit','htmlextra'],
+    reporters: ['cli', 'junit', 'htmlextra'],
     reporter: {
         junit: {
-            export: './reports/report.xml',            
+            export: './reports/report.xml',
         },
         htmlextra: {
-            export: './reports/report.html',             
+            export: './reports/report.html',
         }
     }
 }, function (err) {
     if (err) { throw err; }
     console.log('collection run complete!');
-}));
+});
